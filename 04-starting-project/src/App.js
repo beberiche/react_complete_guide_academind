afcc85d6734404938d648fbdec70411f5ebcb226
beberiche <whdgus269@gmail.com>
2022-06-23 18:40:12 +0900

    220623 section08 finished

--- 04-starting-project/src/App.js
+++ 04-starting-project/src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
+import { useState } from "react";
 
 import AddUser from "./Components/Users/AddUser";
+import UserList from "./Components/Users/UserList.js";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (name, age) => {
+    setUsersList((prevUserList) => [{ name, age, id: Math.random().toString() }, ...prevUserList]);
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={usersList} />
     </div>
   );
 }
