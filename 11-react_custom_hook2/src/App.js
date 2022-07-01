6df76f8e76a31a87515a5933d8a8374d1bd478ff
beberiche <whdgus269@gmail.com>
2022-07-01 18:51:18 +0900

    220701 section 15 finished

---
+++ 11-react_custom_hook2/src/App.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const requestConfig = {
+    url: "https://react-http-2a23a-default-rtdb.firebaseio.com/tasks.json",
+  };
+
+  const httpData = useHttp();
+
+  const { isLoading, error, sendRequest: fetchTasks } = httpData;
+
+  useEffect(() => {
+    const transfromTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(requestConfig, transfromTasks);
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks items={tasks} loading={isLoading} error={error} onFetch={fetchTasks} />
+    </React.Fragment>
+  );
+}
+
+export default App;
