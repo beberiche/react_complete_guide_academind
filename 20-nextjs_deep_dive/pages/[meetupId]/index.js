c14e01a171b8ca8707c7e80ef20b849f8fb85d49
DESKTOP-KVCQHCD\SSAFY <woaol@naver.com>
2022-07-15 00:57:00 +0900

    220715 section23

--- 20-nextjs_deep_dive/pages/[meetupId]/index.js
+++ 20-nextjs_deep_dive/pages/[meetupId]/index.js
@@ -1,50 +1,73 @@
+import { MongoClient, ObjectId } from "mongodb";
+import Head from "next/head";
+
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   // const router = useRouter();
 
   return (
-    <MeetupDetail
-      id="m1"
-      title="A First Meetup"
-      image="https://img.khan.co.kr/news/2020/10/16/2020101601001687000138341.jpg"
-      address="Some Street 5, Some City"
-      description="This is a first meetup"
-    />
+    <>
+      <Head>
+        <title>{props.meetupData.title}</title>
+        <meta name="description" content={props.meetupData.description} />
+      </Head>
+      <MeetupDetail
+        title={props.meetupData.title}
+        image={props.meetupData.image}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
+      />
+    </>
   );
 }
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://JongHyeon:OlSjjGPNiX5jxl8M@cluster0.n2qgwvu.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
+    paths: meetups.map((meetup) => ({
+      params: {
+        meetupId: meetup._id.toString(),
       },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    })),
   };
 }
 
 export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
 
-  console.log(meetupId);
+  const client = await MongoClient.connect(
+    "mongodb+srv://JongHyeon:OlSjjGPNiX5jxl8M@cluster0.n2qgwvu.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
+
+  client.close();
+
   return {
     props: {
       meetupData: {
-        title: "A First Meetup",
-        image:
-          "https://img.khan.co.kr/news/2020/10/16/2020101601001687000138341.jpg",
-        address: "Some Street 5, Some City",
-        description: "This is a first meetup",
-        id: meetupId,
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
       },
     },
   };
--- 20-nextjs_deep_dive/pages/index.js
+++ 20-nextjs_deep_dive/pages/index.js
@@ -1,33 +1,64 @@
+import Head from "next/head";
+import { MongoClient } from "mongodb";
+
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "A First Meetup",
-    image:
-      "https://img.khan.co.kr/news/2020/10/16/2020101601001687000138341.jpg",
-    address: "Some address 5, 12345 Some City",
-    description: "This is a first meetup!",
-  },
-  {
-    id: "m2",
-    title: "A Second Meetup",
-    image:
-      "http://digitalchosun.dizzo.com/site/data/img_dir/2022/06/10/2022061080123_0.jpg",
-    address: "Some address 1, 54321 Some City",
-    description: "This is a Second meetup!",
-  },
-];
+// const DUMMY_MEETUPS = [
+//   {
+//     id: "m1",
+//     title: "A First Meetup",
+//     image:
+//       "https://img.khan.co.kr/news/2020/10/16/2020101601001687000138341.jpg",
+//     address: "Some address 5, 12345 Some City",
+//     description: "This is a first meetup!",
+//   },
+//   {
+//     id: "m2",
+//     title: "A Second Meetup",
+//     image:
+//       "http://digitalchosun.dizzo.com/site/data/img_dir/2022/06/10/2022061080123_0.jpg",
+//     address: "Some address 1, 54321 Some City",
+//     description: "This is a Second meetup!",
+//   },
+// ];
 
 function HomePage(props) {
-  return <MeetupList meetups={props.meetups} />;
+  return (
+    <>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />;
+    </>
+  );
 }
 
 export async function getStaticProps() {
   // fetch data from an API
+  const client = await MongoClient.connect(
+    "mongodb+srv://JongHyeon:OlSjjGPNiX5jxl8M@cluster0.n2qgwvu.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsConnection = db.collection("meetups");
+
+  const meetups = await meetupsConnection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        description: meetup.description,
+        id: meetup._id.toString(),
+      })),
     },
     revalidate: 1,
   };
